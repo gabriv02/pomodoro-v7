568ddc4c0472676f84cd6f0662a013c22082fb4c
Gabriela <a2020120887@estudiantes.upsa.edu.bo>
2021-11-15 02:37:40 -0400

    Primer commit

---
+++ lib/api_service.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'info.dart';
+
+class ApiService {
+  final String url =
+      'https://pomodoroapp20211114235511.azurewebsites.net/api/People';
+
+  // GET FUNCTION
+  Future<List<Info>> getInfo() async {
+    var response = await http.get(Uri.parse(url), headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      print('EXITO: ' + jsonResponse['entries'][0]['API']);
+
+      return (jsonResponse['entries'] as List)
+          .map((e) => Info.fromJSON(e))
+          .toList();
+    }
+    throw Exception('Error al llamar al API');
+  }
+
+  Future<Info> getInfoId(int id) async {
+    var response = await http.get(Uri.parse(url + '/$id'), headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+      //print('EXITO: ' + jsonResponse['entries'][0]['API']);
+
+      return Info.fromJSON(jsonDecode(response.body));
+    }
+    throw Exception('Error al llamar al API');
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,189 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_circular_timer/api_service.dart';
+import 'package:flutter_circular_timer/info.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DemoApp(),
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+    );
+  }
+}
+
+class DemoApp extends StatefulWidget {
+  @override
+  _DemoAppState createState() => _DemoAppState();
+}
+
+class _DemoAppState extends State<DemoApp> {
+  CountDownController _controller = CountDownController();
+  bool _isPause = false;
+
+  TextEditingController idController = TextEditingController();
+  TextEditingController namController = TextEditingController();
+  TextEditingController tarController = TextEditingController();
+
+  ApiService apiService = ApiService();
+  SharedPreferences? sharedPreferences;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pomodoro Timer'),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 10.0),
+          children: [
+            Column(
+              children: [
+                SizedBox(height: 30.0),
+                TextField(
+                  controller: idController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    hintText: "Id",
+                    fillColor: Colors.white,
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  controller: namController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    hintText: "Nombre",
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                TextField(
+                  controller: tarController,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                      ),
+                    ),
+                    hintText: "Tarea",
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Container(
+                  width: 200.0,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      Info data = await apiService
+                          .getInfoId(int.parse(idController.text));
+                      getData(data, idController, namController, tarController);
+                    },
+                    child: Text('Obtener info'),
+                    style: ElevatedButton.styleFrom(primary: Colors.red),
+                  ),
+                ),
+                Center(
+                  child: CircularCountDownTimer(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: MediaQuery.of(context).size.height / 2,
+                    duration: 1500,
+                    fillColor: Colors.redAccent,
+                    controller: _controller,
+                    backgroundColor: Colors.white54,
+                    strokeWidth: 10.0,
+                    strokeCap: StrokeCap.round,
+                    isTimerTextShown: true,
+                    isReverse: true,
+                    onComplete: () {
+                      Alert(
+                              context: context,
+                              title: 'Done',
+                              style: AlertStyle(
+                                isCloseButton: true,
+                                isButtonVisible: false,
+                                titleStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 30.0,
+                                ),
+                              ),
+                              type: AlertType.success)
+                          .show();
+                    },
+                    textStyle: TextStyle(fontSize: 50.0, color: Colors.black),
+                    ringColor: Colors.red,
+                  ),
+                ),
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    setState(() {
+                      if (_isPause) {
+                        _isPause = false;
+                        _controller.resume();
+                      } else {
+                        _isPause = true;
+                        _controller.pause();
+                      }
+                    });
+                  },
+                  icon: Icon(
+                    _isPause ? Icons.play_arrow : Icons.pause,
+                  ),
+                  label: Text(
+                    _isPause ? 'Resume' : 'Pause',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  getData(
+    Info data,
+    TextEditingController id,
+    TextEditingController nam,
+    TextEditingController tar,
+  ) {
+    id.text = data.api.toString();
+    nam.text = data.name.toString();
+    tar.text = data.tarea.toString();
+  }
+}
